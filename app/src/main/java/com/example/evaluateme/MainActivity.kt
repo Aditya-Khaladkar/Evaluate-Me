9c9e0babf98b3a0bdab8a67112a4c4d8565ea330
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-09 13:19:11 +0530

    first commit

---
+++ app/src/main/java/com/example/evaluateme/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.evaluateme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(Runnable {
+
+        },2000)
+    }
+}
---
+++ app/src/main/java/com/example/evaluateme/auth/SignIn.kt
@@ -0,0 +1,12 @@
+package com.example.evaluateme.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluateme.R
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
