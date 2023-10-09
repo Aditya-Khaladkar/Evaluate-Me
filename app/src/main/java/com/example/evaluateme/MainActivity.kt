477bc4397dc5b248304c5c64ee827b9fa600ec36
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-09 13:20:40 +0530

    Implemented Handler thread to navigate

--- app/src/main/java/com/example/evaluateme/MainActivity.kt
+++ app/src/main/java/com/example/evaluateme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.evaluateme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.evaluateme.auth.SignIn
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Handler().postDelayed(Runnable {
-
-        },2000)
+            startActivity(Intent(this, SignIn::class.java))
+            finish()
+        }, 2000)
     }
 }
