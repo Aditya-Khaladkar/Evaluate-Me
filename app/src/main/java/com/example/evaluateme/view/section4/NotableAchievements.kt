ae17694e08883d8bd1e4d74c0593836e9a55fb22
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-11 00:24:13 +0530

    Add: section 4 done

---
+++ app/src/main/java/com/example/evaluateme/view/section4/NotableAchievements.kt
@@ -0,0 +1,12 @@
+package com.example.evaluateme.view.section4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluateme.R
+
+class NotableAchievements : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notable_achievements)
+    }
+}
