d1dceb0bc9884ff3b2f417cd51c8e1f66e734b2d
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-10 13:12:57 +0530

    Add: Added Gender Question UI

---
+++ app/src/main/java/com/example/evaluateme/view/section1/NameAndEmail.kt
@@ -0,0 +1,12 @@
+package com.example.evaluateme.view.section1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluateme.R
+
+class NameAndEmail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_and_email)
+    }
+}
---
+++ app/src/main/java/com/example/evaluateme/view/section2/QuesGender.kt
@@ -0,0 +1,12 @@
+package com.example.evaluateme.view.section2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluateme.R
+
+class QuesGender : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ques_gender)
+    }
+}
