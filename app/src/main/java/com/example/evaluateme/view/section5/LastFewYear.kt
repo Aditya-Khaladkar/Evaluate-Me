66e354295219c78337221f09b65e899ad80831d8
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-13 10:02:58 +0530

    Add: complete section 5

---
+++ app/src/main/java/com/example/evaluateme/view/section5/LastFewYear.kt
@@ -0,0 +1,12 @@
+package com.example.evaluateme.view.section5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluateme.R
+
+class LastFewYear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last_few_year)
+    }
+}
