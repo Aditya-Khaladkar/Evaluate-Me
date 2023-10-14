b3c643cc2f5584ca11c71c3c14f5e271c3097dad
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-14 16:57:43 +0530

    Add: Added auth with shared pref

--- app/src/main/java/com/example/evaluateme/auth/SignIn.kt
+++ app/src/main/java/com/example/evaluateme/auth/SignIn.kt
@@ -1,12 +1,26 @@
 package com.example.evaluateme.auth
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.evaluateme.R
+import com.example.evaluateme.databinding.ActivitySignInBinding
 
 class SignIn : AppCompatActivity() {
+    lateinit var binding: ActivitySignInBinding
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+
+        binding.btnSignIn.setOnClickListener {
+            val email: String = binding.logEmail.text.toString()
+            val password: String = binding.logPassword.text.toString()
+        }
     }
 }
--- app/src/main/java/com/example/evaluateme/auth/SignUp.kt
+++ app/src/main/java/com/example/evaluateme/auth/SignUp.kt
@@ -1,12 +1,41 @@
 package com.example.evaluateme.auth
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.evaluateme.R
+import com.example.evaluateme.databinding.ActivitySignUpBinding
+import com.example.evaluateme.view.section1.NameAndEmail
 
 class SignUp : AppCompatActivity() {
+    lateinit var sharedPref: SharedPreferences
+    lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        sharedPref = getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+
+        binding.btnSignUp.setOnClickListener {
+            val username: String = binding.regUsername.text.toString()
+            val email: String = binding.regEmail.text.toString()
+            val password: String = binding.regPassword.text.toString()
+
+            val editor = sharedPref.edit()
+            editor.putString("username", username)
+            editor.putString("email", email)
+            editor.putString("password", password)
+            editor.apply()
+
+            Toast.makeText(this, "Registration Successful", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, NameAndEmail::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
