e518e6cd7674c0aafffb474375e06ea20f4b8822
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-10-14 16:36:04 +0530

    Mod: restructured code

---
+++ app/src/main/java/com/example/evaluateme/view/section6/BusinessAchieved.kt
@@ -0,0 +1,12 @@
+package com.example.evaluateme.view.section6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evaluateme.R
+
+class BusinessAchieved : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business_achieved)
+    }
+}
